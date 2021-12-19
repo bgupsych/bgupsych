e532d5f1c1aba3a1f851c1b84d17fc040b3c297b
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-19 17:15:02 +0200

    Update ssmodel.R

--- R/ssmodel.R
+++ R/ssmodel.R
@@ -1,12 +1,21 @@
 #' Generates the SST/Res/Reg, Rsq and Adj Rsq of lm model.
 #' @export
 #' @description Make sure to create your \code{lm} object first. Put your object in the function and see how magic happens.
-#' @examples x <- lm(Volume~.,trees)
+#' @examples x <- lm(Happiness.Score~.,Happiness.Score)
 #' ssmodel(x)
+#' # This model predicts Happiness score by all other variables in the data frame.
+#'
+#' y <- lm(Bsum~age+sat,data=depression)
+#' ssmodel(y)
+#' # This model only uses age and sat to predict Bsum
+#' @param model lm() object
 #' @seealso What happens if you use the mean in your model as prediction?
-#' \code{ssmodel(lm(trees$Volume~rep(mean(trees$Volume),nrow(trees))))}
-#' # This is a hard-to-read input that contains a repetition vector of the mean value of Volume for as many times as the  number of rows in the data,
-#' # then these values (actually, constant) are used to predict Volume. See for yourself that SSRes=SST and SSReg=0.
+#'
+#' \code{Mean <- rep(mean(depression$Bsum),nrow(depression))}
+#' # a repetition of the Bsum's mean, times the number of observations in 'depression'
+#' \code{ssmodel(lm(Bsum~Mean,depression))}
+#
+#' # The mean value is used to predict Bsum. See for yourself that SSRes=SST and SSReg=0.
 
 ssmodel <- function(model){
   SST = round(var(model$model[,1])*
@@ -17,6 +26,6 @@ ssmodel <- function(model){
   lm_m <- summary(model)
   adrsq=round(lm_m$adj.r.squared,4)
   pred=names(model$coefficients[-1])
-  return(c(SST=SST,SSRes=SSRes,SSReg=SSReg,
+  return(data.frame(SST=SST,SSRes=SSRes,SSReg=SSReg,
            Rsq=Rsq,"Adj.Rsq"=adrsq,Predictor=pred))
 }
