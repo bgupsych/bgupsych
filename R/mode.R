68dfc7058318bf3697ec3078d45b1b11191be01a
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-19 17:29:21 +0200

    Update mode.R

--- R/mode.R
+++ R/mode.R
@@ -7,6 +7,12 @@
 #' mode(x) ## 13
 #' y <- c(x,14)
 #' mode(y) ## 13, 14
-mode <- function(x) {
-  x <- as.numeric(names(table(x))[table(x)==max(table(x))])
-  return(x)}
+#' mode(y,freq=TRUE)
+#' @param x A numeric vector
+#' @param freq A logical argument. Adds the frequency of your mode, default is FALSE
+#' @returns Most frequent value, and if freq is set to TRUE, a how many times it appears
+mode <- function(x,freq=F) {
+  ans <- as.numeric(names(table(x))[table(x)==max(table(x))])
+  frq=head(sort(table(x),decreasing = T),1)
+  ifelse(freq,return(c(ans,freq=as.numeric(frq))),return(ans))
+}
