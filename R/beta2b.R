1864f8d310de2b709cb4d9c02fdbfaabcbbb1ff3
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-19 17:16:48 +0200

    Update beta2b.R

--- R/beta2b.R
+++ R/beta2b.R
@@ -5,9 +5,12 @@
 #' @param x The x variable (Undependent).
 #' @param y The y variable (Dependent).
 #' @returns raw Mekadem b
-#' @examples beta2b(beta=1.45, x=trees$Height, y=trees$Volume)
-#' ## 3.740674
-#' ## For every increase of 1 unit in Height, Volume increases by 3.740674 units.
+#' @examples
+#' model = lm(Bsum~sat+age,data=depression)
+#' by using \code{\link{lm.beta}} we get sat~0.504, and age~0.467.
+#' We want to convert them back to raw b:
+#' sat: beta2b(beta=0.5038211, x=depression$sat, y=depression$Bsum)
+#' ## 0.7899105
+#' age: beta2b(0.4673743,depression$age, y=depression$Bsum)
+#' ## 0.4072746
 beta2b <- function(beta,x,y)(beta*sd(y)/sd(x))
-
-
