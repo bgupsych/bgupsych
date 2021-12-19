943d635b9c25afc6cd24758c04281093ce57dc98
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-20 00:46:14 +0200

    documanting data

---
+++ R/regression1.R
@@ -0,0 +1,11 @@
+#' A data set to practice a simple linear regression
+#'
+#' A simple dataframe to practice simple linear regression with.
+#' @docType data
+#'
+#' @usage data(regression1)
+#'
+#'
+#' @references BGU, Psychology, Regression
+#'
+"regression1"
