9b3aead56a503b049d188f803e63e34ce9ee1e02
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-21 01:58:07 +0200

    Add files via upload

---
+++ R/big5.R
@@ -0,0 +1,16 @@
+#' Big 5 personality traits
+#'
+#' A dataset containing the score in each of the personality traits, age and gender for each observation.
+#'
+#' @format A data frame with 250 rows and 7 variables:
+#' \describe{
+#'   \item{OCEAN}{Score in each of the 5 personality traits}
+#'   \item{age} {Individual's age}
+#'   \{gender} {Individual's gender}
+#' }
+#' @references BGU, Psychology, Regression
+#' @source \url{https://www.psychologytoday.com/intl/basics/big-5-personality-traits}
+#' @docType data
+#'
+#' @usage data(big5)
+"big5"
