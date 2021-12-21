de1d106558232234d6f9482fa1c22f8cc5fdfb44
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-22 01:50:17 +0200

    Update Pascal.R

--- R/Pascal.R
+++ R/Pascal.R
@@ -3,7 +3,9 @@
 #' It is highly linked with the \code{\link{choose}} function.
 #' @export
 #' @importFrom purrr accumulate
-#' @source \url{https://www.mathsisfun.com/pascals-triangle.html}
+#' @seealso \url{https://www.mathsisfun.com/pascals-triangle.html}
+#' @source This function makes use of \code{\link{accumulate}} from the \code{\link{purrr} package.
+#' \url{https://purrr.tidyverse.org/reference/index.html}
 #' @examples Pascal(4)
 #' # if you count k indexes (after 1) in row n (after 1st), you get the result of choost(n,k).
 #' ## in [[5]], which is row n=4, k=2 index is 6. this is the binomal result of choose(4,2).
