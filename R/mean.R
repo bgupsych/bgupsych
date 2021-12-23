af532cc877ec4b3777a1e639a7e3f420bc33873a
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-23 16:56:37 +0200

    Delete R directory

--- R/mean.R
+++
@@ -1,9 +0,0 @@
-#' Defaults mean() to na.rm=TRUE
-#' @export
-#' @description Simply changes the default argument to remove NA.
-#' @examples x <- c(4,5,6,NA,6)
-#' mean(x)
-#' ## 5.25
-mean <- function(x, ..., na.rm = TRUE) {
-  base::mean(x, ..., na.rm = na.rm)
-}
