49202abd35660a552ce2dd2d11ab9bd769d76f16
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-23 17:16:27 +0200

    Add files via upload

---
+++ R/happiness.R
@@ -0,0 +1,13 @@
+#' Happiness Score data
+#'
+#' A dataset containing the score in each of the variables measured.
+#'
+#' @format A data frame with 155 rows and 7 variables:
+#' \describe{
+#'   \item{Happiness.Score}{Happiness score}
+#' }
+#' @references BGU, Psychology, Regression
+#' @docType data
+#'
+#' @usage data(happiness)
+"happiness"
