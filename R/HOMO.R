2d8885680e15a4e10e5c39b8ce35bdf48904431f
bgupsych <95340431+bgupsych@users.noreply.github.com>
2021-12-28 21:44:27 +0200

    Add files via upload

---
+++ R/HOMO.R
@@ -0,0 +1,32 @@
+#' Homoscedasticity visualization
+#' @description A nice visualization to asses whether a homoscedasticity is present
+#' @export
+#' @param lm_object A linear model \code{\link{lm}}.
+#' @param res Logical to detremine whether to use raw DV, or set the y scale to the residuals. default is set to TRUE (show residualized y axis).
+#' @param transparence Controls the transparency of the points. Default as 0.8.
+#' @returns \code{\link{summary}} of the lm object.
+#' @returns  \code{graph} (print only).
+#' @note Though you can use a multi-regression model, the X axis will show the first predictor only.
+#' @seealso \url{https://www.youtube.com/watch?v=zRklTsY9w9c}
+#' @examples \dontshow {attach(depression)}
+#' model <- lm(Bsum~m_years)
+#' HOMO(model)
+#' HOMO(model,res=FALSE,transparence=.1)
+#' \dontshow{detach(depression)}
+HOMO <- function(lm_object,res=T,transparence=0.8){
+
+  transparence <- abs(transparence)
+  Predictor <- lm_object$model[,2]
+  if(res){DV=resid(lm_object)}
+  else{DV=lm_object$model[,1]}
+  m=coef(lm_object)
+  if(res){s=0}else{s=m[2]}
+  if(res){a=0}else{a=m[1]}
+  g=ggplot2::ggplot(lm_object$model,aes(x=Predictor,y=DV))+
+    geom_point(aes(x=Predictor,y=DV,color=as.factor(Predictor)),alpha=transparence)+
+    geom_abline(slope = s,intercept = a,size=transparence*1.8+.05,color="navy")+
+    theme(legend.position = "none")
+  print(g)
+  return(summary(lm_object))
+}
+
