44ad568e1f100ace118607f0f29ac041b46904d9
bgupsych <95340431+bgupsych@users.noreply.github.com>
2022-01-10 05:03:29 +0200

    Update ml.R
    adding ggplot to ml

--- R/ml.R
+++ R/ml.R
@@ -12,19 +12,20 @@
 #' @param k Number of successes observed in all trials.
 #' @param L The probability for success in each trial.
 #' @param round Number of decimals desired. default is set to 4.
+#' @param geom You can choose out of 3 \code{geoms} to visualise your results: \code{\link{geom_jitter}} (default), \code{\link{geom_col}}, or \code{\link{geom_line}}.
 #' @examples ml(k=7,n=10,L=0.7) # see full print for this one.
 #'
 #' @examples You can change the decimals by adjusting the round argument.
 #'  ml(7,10,0.7,round=3)
-#'
+#' @examples ml(7,10,L=probs,geom="col")
 #' @examples You can even test the likelihood of numerous probabilities.
 #'  probs <- seq(0,1,length=10) #10 values between 0 and 1
 #'  ml(7,10,L=probs)
 #' @return \code{\link{data.frame}} with the probability tested and its likelihood.
 #'   \code{print}: A verbal print, modified to percentage.
-
-
-ml <- function(k,n,L,round=4){
+#'   \code{graph} of the Likelihood for each probability.
+ml <- function(k,n,L,round=4,geom="jitter"){
+  require("ggplot2")
   ml <- dbinom(k,n,L)
   df <- data.frame(prob=round(L,round),Likelihood=round(ml,round))
   dff <- df[order(df[,2],decreasing = T),,]
@@ -36,5 +37,11 @@ ml <- function(k,n,L,round=4){
         " is the probability for succsess in each trial.\nIt has a ",
         dff[1,2]*100,"% chance.\n\n",sep="")
   }
+  gdff=dff[dff[,2]>0,]
+  if(geom=="jitter"){print(ggplot(gdff,aes(x=prob,y=Likelihood))+
+                              geom_jitter())} else if(geom=="col"){
+                                print(ggplot(gdff,aes(x=prob,y=Likelihood))+
+                                        geom_col())} else {print(ggplot(gdff,aes(x=prob,y=Likelihood))+
+                                                                   geom_line())}
   return(dff)
 }
