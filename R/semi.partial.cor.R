616f98dd888aaf4bf35c3b004c345308a9a72b24
bgupsych <95340431+bgupsych@users.noreply.github.com>
2022-01-10 05:08:04 +0200

    Update semi.partial.cor.R

--- R/semi.partial.cor.R
+++ R/semi.partial.cor.R
@@ -1,5 +1,6 @@
 #' Partial and Semi-partial correlations
 #' @export
+#' @import ppcor
 #' @description In the Partial correlation, the controlled variable is removed from both DV and UDV,
 #' While in Semi-partial, controlled is removed only from the UDV, and so it is very much alike the Beta (direct effect).
 #' @details Be sure to understand the theoretical meaning of semi and partial correlation.
@@ -7,20 +8,25 @@
 #'  Partial correlation is the correlation of two
 #'  variables while controlling a third.
 #'   SP holds constant the controlled variable only for the UDV (predictor).
-#' @seealso \code{\link{ppcor}} for more options.
+#'   Read more here \url{https://www.youtube.com/watch?v=OpAf4N582bA}
+#' @seealso This function runs \code{\link{ppcor}}, check it out for more options.
 #' @examples \dontshow{attach(depression)}
 #' semi.partial.cor(DV=Bsum,x.predictor=m_years, x.controlled=age)
 #' # We correlate 'Bsum' and 'm_years', holding 'age' constant. In SP it's been removed from only the predicting variable,
 #' # whilst in partial from both.
+#' \dontshow{detach(depression)}
 #' @note Partial will always be Greater than/Equal to Semi-partial.
-#' @returns \code{data.frame} with the \code{Rsq} of partial and semi-partial, sample size and DF.
+#' @returns \code{data.frame} with the \code{estimate} for both semi-partial & partial correlations, and its \code{p-value}.
 
 semi.partial.cor <- function(DV,x.predictor,x.controlled){
-  partial <- cor(resid(lm(DV~x.controlled)),
-                 resid(lm(x.predictor~x.controlled)))
-  semi_partial <- cor(DV,resid(lm(x.predictor~x.controlled)))
-  return(data.frame(semi_partial=semi_partial,partial=partial,
-                    df=length(DV)-3,n=length(DV)))
-
+  require(ppcor)
+  DV=x
+  x.predictor=y
+  x.controlled=z
+  p=ppcor::pcor.test(x,y,z)
+  sp=ppcor::spcor.test(x,y,z)
+  psp=data.frame(type=c("partial","semipartial"),
+                 estimate=c(as.numeric(p[1]),as.numeric(sp[1])),
+                 p.value=c(as.numeric(p[2]),as.numeric(sp[2])))
+  return(psp)
 }
-
