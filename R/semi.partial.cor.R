17db217c54893877c0b334dc0ad8e43ed85602a0
bgupsych <95340431+bgupsych@users.noreply.github.com>
2022-01-27 20:15:46 +0200

    Update semi.partial.cor.R

--- R/semi.partial.cor.R
+++ R/semi.partial.cor.R
@@ -11,20 +11,20 @@
 #'   Read more here \url{https://www.youtube.com/watch?v=OpAf4N582bA}
 #' @seealso This function runs \code{\link{ppcor}}, check it out for more options.
 #' @examples \dontshow{attach(depression)}
-#' semi.partial.cor(DV=Bsum,x.predictor=m_years, x.controlled=age)
+#' semi.partial.cor(x1=Bsum,x2=m_years, x.controlled=age)
 #' # We correlate 'Bsum' and 'm_years', holding 'age' constant. In SP it's been removed from only the predicting variable,
 #' # whilst in partial from both.
 #' \dontshow{detach(depression)}
 #' @note Partial will always be Greater than/Equal to Semi-partial.
 #' @returns \code{data.frame} with the \code{estimate} for both semi-partial & partial correlations, and its \code{p-value}.
-
-semi.partial.cor <- function(DV,x.predictor,x.controlled){
+#' @param x1 A numeric vector you want to correlate with x2.
+#' @param x2 A numeric vector you want to correlate with x1.
+#' @param x.controlled A numeric vector you want to hold constant.
+semi.partial.cor <- function(x1,x2,x.controlled){
   require(ppcor)
-  x=DV
-  y=x.predictor
   z=x.controlled
-  p=ppcor::pcor.test(x,y,z)
-  sp=ppcor::spcor.test(x,y,z)
+  p=ppcor::pcor.test(x1,x2,z)
+  sp=ppcor::spcor.test(x1,x2,z)
   psp=data.frame(type=c("partial","semipartial"),
                  estimate=c(as.numeric(p[1]),as.numeric(sp[1])),
                  p.value=c(as.numeric(p[2]),as.numeric(sp[2])))
