9868186c3f635c7b88f5b08766f8f14afb33dfc5
bgupsych <95340431+bgupsych@users.noreply.github.com>
2022-01-27 20:16:40 +0200

    Update draw.R

--- R/draw.R
+++ R/draw.R
@@ -6,6 +6,8 @@
 #' @param cumulative a logical. If TRUE: draws a CDF. default is set to FALSE.
 #' @param sort.prob a logical. Concerns the data frame returned, if TRUE: sort by probability (in decreasing order), Otherwise same order as input of X. default is set to FALSE.
 #' @param round How many decimals to show. Default is set to 3.
+#' @param show.label A logical. Default (TRUE) prints probability labels on the plot.
+#' @param nudge When \code{show.label} is TRUE, you can adjust the location of the labels by inputing a 2 length vector to adjust the label position (a single values works too). Usualy no adjustment is needed.
 #' @returns \code{graph} (Print only).
 #' @return \code{Tohelet and Shonut} (Print only).
 #' @return \code{data.frame} of X and their probs (use \code{sort.prob} to change order method).
@@ -20,7 +22,7 @@
 #' ## Even when using negative probability, it will work.
 #' @seealso  For a fully detailed explanation about the differences between distributions please visit \url{https://www.researchgate.net/post/What-is-the-difference-between-probability-distribution-function-and-probability-density-function}
 #'
-draw <- function(X,p,cumulative=F,sort.prob=F,round=3){
+draw <- function(X,p,cumulative=F,sort.prob=F,round=3,show.label=TRUE,nudge=0){
   require("ggplot2")
   P <- abs(p)
   len=c("Please make sure there's either 1 probability (like in a dice),\n  or same length as X (Mishtane Mikri)")
@@ -31,19 +33,24 @@ draw <- function(X,p,cumulative=F,sort.prob=F,round=3){
     P[i]=sum(P[i],P[i-1])}}
 
   a=data.frame(X=X,prob=round(P,round))
-  print(ggplot(a,aes(X,prob,fill=prob))+
+  g=ggplot(a,aes(X,prob,fill=prob))+
           geom_col()+
           geom_label(aes(label=round(prob,round)),
                      color="white",alpha=0.6,nudge_y = 0.02)+
           scale_x_continuous(breaks = X)+
           theme(legend.position = "none")+
-    scale_y_continuous(breaks = NULL))
+          scale_y_continuous(breaks = NULL)
+
   cat("Tohelet:", t,"\n")
   cat("Shonut:",sum((X-t)^2*ogp),"\n")
   if(sort.prob){a=a[order(a[,2],decreasing = T), ,]}
-  if(sum(ogp)>1){cat("\nWARNING:\nProbabilities add up tp more than 1.\n")}
-  return(a)
 
+if(sum(ogp)>1){cat("\nWARNING:\nProbabilities add up tp more than 1.\n")}
+  if(sort.prob){a=a[order(a[,2],decreasing = T), ,]}
+  ifelse(show.label,print(g+geom_label(aes(label=round(prob,round)),
+                                       alpha=0.6,nudge_x = nudge[1],nudge_y = nudge[2],color="white")),print(g))
+
+  return(a)
 }
 
 #' Draws a binomal distribution
@@ -55,6 +62,8 @@ draw <- function(X,p,cumulative=F,sort.prob=F,round=3){
 #' @param cumulative a logical. If TRUE: draws a CDF. default is set to FALSE.
 #' @param sort.prob a logical. Concerns the data frame returned, if TRUE: sort by probability (in decreasing order), Otherwise same order as input of X. default is set to FALSE.
 #' @param round How many decimals to show. Default is set to 3.
+#' @param show.label A logical. Default (TRUE) prints probability labels on the plot.
+#' @param nudge When \code{show.label} is TRUE, you can adjust the location of the labels by inputing a 2 length vector to adjust the label position (a single values works too). Usualy no adjustment is needed.
 #' @returns \code{graph} (Print only).
 #' @return \code{Tohelet and Shonut} (Print only).
 #' @return \code{data.frame} of X and their probs (use \code{sort.prob} to change order method).
@@ -67,7 +76,7 @@ draw <- function(X,p,cumulative=F,sort.prob=F,round=3){
 #' ## Even when using negative probability, it will work.
 #' @seealso  For a fully detailed explanation about the differences between distributions please visit \url{https://www.researchgate.net/post/What-is-the-difference-between-probability-distribution-function-and-probability-density-function}
 #'
-draw.binom <- function(X,n=max(X),p,cumulative=F,sort.prob=F,round=3){
+draw.binom <- function(X,n=max(X),p,cumulative=F,sort.prob=F,round=3,show.label=TRUE,nudge=0){
   require("ggplot2")
   p <- abs(p)
   errp=c("\nProbability MUST be 0 >= p <= 1")
@@ -82,13 +91,14 @@ draw.binom <- function(X,n=max(X),p,cumulative=F,sort.prob=F,round=3){
   g=ggplot(a,aes(x=X,y=prob,fill=prob))+geom_col()+
     theme(legend.position = "none")+
     scale_y_continuous(breaks = NULL)+
-    scale_x_continuous(breaks = X)+
-    geom_label(aes(label=round(prob,round)),
-               color="white",alpha=0.6,nudge_y = 0.02)
+    scale_x_continuous(breaks = X)
+
   cat("Tohelet =",toh,"\n")
   cat("Shonut =",sho,"\n\n")
   if(sort.prob){a=a[order(a[,2],decreasing = T), ,]}
-  print(g)
+  ifelse(show.label,print(g+geom_label(aes(label=round(prob,round)),
+                                       alpha=0.6,nudge_x = nudge[1],nudge_y = nudge[2],color="white")),print(g))
+
   return(a)
 }
 
@@ -100,6 +110,8 @@ draw.binom <- function(X,n=max(X),p,cumulative=F,sort.prob=F,round=3){
 #' @param cumulative a logical. If TRUE: draws a CDF. default is set to FALSE.
 #' @param decreasing a logical. Concerns the data frame returned, if TRUE: decreasing order of probabilities, Otherwise same order as input of X. default is set to FALSE.
 #' @param round How many decimals to show. Default is set to 3.
+#' @param show.label A logical. Default (TRUE) prints probability labels on the plot.
+#' @param nudge When \code{show.label} is TRUE, you can adjust the location of the labels by inputing a 2 length vector to adjust the label position (a single values works too). Usualy no adjustment is needed.
 #' @returns \code{graph} (Print only).
 #' @return \code{Tohelet and Shonut} (Print only).
 #' @return \code{data.frame} of X and their probs (use \code{decreasing} to change order method, in case random numbers are assigned).
@@ -109,7 +121,7 @@ draw.binom <- function(X,n=max(X),p,cumulative=F,sort.prob=F,round=3){
 #' @examples draw.geom(x,p=0.3,cumulative=TRUE)
 #' @seealso  For a fully detailed explanation about the differences between distributions please visit \url{https://www.researchgate.net/post/What-is-the-difference-between-probability-distribution-function-and-probability-density-function}
 #'
-draw.geom <- function(X,p,cumulative=F,decreasing=F,round=3){
+draw.geom <- function(X,p,cumulative=F,decreasing=F,round=3,show.label=TRUE,nudge=0){
   require("ggplot2")
   p <- abs(p)
   X <- abs(X)
@@ -128,14 +140,16 @@ draw.geom <- function(X,p,cumulative=F,decreasing=F,round=3){
     geom_point()+geom_col(alpha=0.7)+
     theme(legend.position = "none")+
     scale_y_continuous(breaks = NULL)+
-    scale_x_continuous(breaks = X)+
-    geom_label(aes(label=round(prob,round)),
-               color="white",alpha=0.6,nudge_y = 0.02)+
+    scale_x_continuous(breaks = X)
     xlab("MISHTANE MIKRI")+labs(caption = "This function uses x=1 as succsses in 1st try")
-  cat("Tohelet =",toh,"\n")
+
+    cat("Tohelet =",toh,"\n")
   cat("Shonut =",sho,"\n")
   if(decreasing){a=a[order(a[,2],decreasing = T), ,]}
-  print(g)
+  if(length(nudge==1)){nudge=c(nudge,nudge)}
+  ifelse(show.label,print(g+geom_label(aes(label=round(prob,round)),
+                                       alpha=0.6,nudge_x = nudge[1],nudge_y = nudge[2],color="white")),print(g))
+
   return(a)
 }
 
@@ -147,16 +161,18 @@ draw.geom <- function(X,p,cumulative=F,decreasing=F,round=3){
 #' @param cumulative a logical. If TRUE: draws a CDF. default is set to FALSE.
 #' @param sort.prob a logical. Concerns the data frame returned, if TRUE: sort by probability (in decreasing order), Otherwise same order as input of X. default is set to FALSE.
 #' @param round How many decimals to show. Default is set to 3.
+#' @param show.label A logical. Default (TRUE) prints probability labels on the plot.
+#' @param nudge When \code{show.label} is TRUE, you can adjust the location of the labels by inputing a 2 length vector to adjust the label position (a single values works too). Usualy no adjustment is needed.
 #' @returns \code{graph} (Print only).
 #' @return \code{Tohelet and Shonut} (Print only).
-#' @return \code{data.frame} of X and their probs (use \code{decreasing} to change order method, in case random numbers are assigned).
+#' @return \code{data.frame} of X and their probs (use \code{sort.prob} to change order method, if random numbers are assigned).
 #' @note Very important: Unlike usual input in \code{\link{dgeom}}, please refer to 1 as success in 1st attempt.
 #' @examples x <- c(1:5)
 #' draw.pois(x,lam=0.3)
 #' @examples draw.pois(x,lam=1.2,cumulative=TRUE)
 #' @seealso  For a fully detailed explanation about the differences between distributions please visit \url{https://www.researchgate.net/post/What-is-the-difference-between-probability-distribution-function-and-probability-density-function}
 #'
-draw.pois <- function(X,lam,cumulative=F,sort.prob=F,round=3){
+draw.pois <- function(X,lam,cumulative=F,sort.prob=F,round=3,show.label=TRUE,nudge=0){
   require("ggplot2")
   lam <- abs(lam)
   X <- abs(X)
@@ -169,14 +185,17 @@ draw.pois <- function(X,lam,cumulative=F,sort.prob=F,round=3){
   g=ggplot(a,aes(x=X,y=prob,fill=prob))+geom_col()+
     theme(legend.position = "none")+
     scale_y_continuous(breaks = NULL)+
-    scale_x_continuous(breaks = X)+
-    geom_label(aes(label=round(prob,round)),
-               color="white",alpha=0.6,nudge_y = 0.02)+
-    xlab("MISHTANE MIKRI")
+    scale_x_continuous(breaks = X)
+
+  xlab("MISHTANE MIKRI")
   cat("Tohelet =",lamog,"\n")
   cat("Shonut =",lamog,"\n")
   if(sort.prob){a=a[order(a[,2],decreasing = T), ,]}
-  print(g)
+
+  if(length(nudge==1)){nudge=c(nudge,nudge)}
+  ifelse(show.label,print(g+geom_label(aes(label=round(prob,round)),
+   alpha=0.6,nudge_x = nudge[1],nudge_y = nudge[2],color="white")),print(g))
+
   return(a)
 }
 
@@ -189,6 +208,8 @@ draw.pois <- function(X,lam,cumulative=F,sort.prob=F,round=3){
 #' @param sort.prob a logical. Concerns the data frame returned, if TRUE: sort by probability (in decreasing order), Otherwise same order as input of X. default is set to FALSE.
 #' @param round How many decimals to show. Default is set to 3.
 #' @param x.range a vector of two numbers The probability of occurrence between these two values. Use \code{c(min,max)}.
+#' @param show.label A logical. Default (TRUE) prints probability labels on the plot.
+#' @param nudge When \code{show.label} is TRUE, you can adjust the location of the labels by inputing a 2 length vector to adjust the label position (a single values works too). Usualy no adjustment is needed.
 #' @returns \code{graph} (Print only).
 #' @return \code{Tohelet and Shonut} (Print only).
 #' @return \code{data.frame} of X and their probs (use \code{sort.prob} to change order method, in case random numbers are assigned).
@@ -199,7 +220,7 @@ draw.pois <- function(X,lam,cumulative=F,sort.prob=F,round=3){
 #' @examples draw.exp(0:5,rate=1/3,x.range=c(3,5))
 #' ## Prob for successes between this range (3rd and 5th), given P~rate of 1/3.
 #'
-draw.exp <- function(X,rate,cumulative=T,sort.prob=F,round=3,x.range=NULL){
+draw.exp <- function(X,rate,cumulative=T,sort.prob=F,round=3,x.range=NULL,show.label=TRUE,nudge=0){
   require("ggplot2")
   rate <- abs(rate)
   X <- abs(X)
@@ -211,21 +232,22 @@ draw.exp <- function(X,rate,cumulative=T,sort.prob=F,round=3,x.range=NULL){
 
   a=data.frame(x=X,prob=round(prob,round))
   g=ggplot(a,aes(x=X,y=prob,fill=prob))+geom_col(alpha=.4)+
-    geom_line(aes(x=X,y=prob,size=1.2))+
+    geom_col(alpha=.4)+
+    geom_smooth(se=F,formula = 'y ~ x',method = 'loess')+
     theme(legend.position = "none")+
     scale_y_continuous(breaks = NULL)+
     scale_x_continuous(breaks = X)+
-    geom_label(aes(label=round(prob,round)),
-               color="white",alpha=0.6,nudge_y = 0.02)+
     xlab("MISHTANE MIKRI")+ylab("density")+
     labs(caption = "for P(x) use cumulative=TRUE ")
+
   cat("Tohelet (and SD) =",toh,"\n")
   cat("Shonut =",sho,"\n")
   if(sort.prob){a=a[order(a[,2],decreasing = T), ,]}
   if(is.vector(x.range)) {cat("Probability for event between",
-                              x.range[1],"and",x.range[2],"is",round(abs(pexp(x.range[1],rate)
-                                                                         -pexp(x.range[2],rate)),round),"\n")}
-  print(g)
+                              x.range[1],"and",x.range[2],"is",round(abs(pexp(x.range[1],rate)-pexp(x.range[2],rate)),round),"\n")}
+  if(length(nudge==1)){nudge=c(nudge,nudge)}
+  ifelse(show.label,print(g+geom_label(aes(label=round(prob,round)),
+                                       alpha=0.8,nudge_x = nudge[1],nudge_y = nudge[2],color="white")),print(g))
   return(a)
 }
 
@@ -235,10 +257,12 @@ draw.exp <- function(X,rate,cumulative=T,sort.prob=F,round=3,x.range=NULL){
 #' @param X Mishtane Mikri. Can be a vector of values.
 #' @param mean The mean of the distribution
 #' @param sd The standard deviation of the distribution
-#' @param cumulative a logical. If FALSE: draws a PMF default is set to TRUE
-#' @param sort.prob a logical. Concerns the data frame returned, if TRUE: sort by probability (in decreasing order), Otherwise same order as input of X. default is set to FALSE.
+#' @param cumulative A logical. If FALSE: draws a PMF default is set to TRUE
+#' @param sort.prob A logical. Concerns the data frame returned, if TRUE: sort by probability (in decreasing order), Otherwise same order as input of X. default is set to FALSE.
 #' @param round How many decimals to show. Default is set to 3.
 #' @param x.range a vector of two numbers The probability of occurrence between these two values. Use \code{c(min,max)}.
+#' @param show.label A logical. Default (TRUE) prints probability labels on the plot.
+#' @param nudge When \code{show.label} is TRUE, you can adjust the location of the labels by inputing a 2 length vector to adjust the label position (a single values works too). Usualy no adjustment is needed.
 #' @returns \code{graph} (Print only).
 #' @return \code{Tohelet and Shonut} (Print only).
 #' @return \code{data.frame} of X and their probs (use \code{sort.prob} to change order method.
@@ -250,31 +274,31 @@ draw.exp <- function(X,rate,cumulative=T,sort.prob=F,round=3,x.range=NULL){
 #' # In a distribution of mean=5 and sd=1.5, print values 0-10. Also what is the probability between 3.5 and 7?
 #' @seealso  For a fully detailed explanation about the differences between distributions please visit \url{https://www.researchgate.net/post/What-is-the-difference-between-probability-distribution-function-and-probability-density-function}
 #'
-draw.norm <- function(X,mean=0,sd=1,cumulative=T,sort.prob=F,round=3,x.range=NULL){
+draw.norm <- function(X,mean=0,sd=1,cumulative=T,sort.prob=F,round=3,x.range=NULL,show.label=TRUE,nudge=0){
   require("ggplot2")
   toh=mean
   sho=sd^2
 
   if(cumulative){prob=pnorm(X,mean,sd)}
   else {prob=dnorm(X,mean,sd)}
-
   a=data.frame(x=X,prob=round(prob,round))
   g=ggplot(a,aes(x=X,y=prob,fill=prob))+geom_col(alpha=.4)+
-    geom_line(aes(x=X,y=prob,size=1.2))+
+    geom_smooth(se=F,formula = 'y ~ x',method = 'loess')+
     theme(legend.position = "none")+
     scale_y_continuous(breaks = NULL)+
     scale_x_continuous(breaks = c(round(min(X),round),
                                   round(mean(X),round),round(max(X),round),toh))+
-    geom_label(aes(label=round(prob,round)),
-               color="white",alpha=0.6,nudge_y = 0.008)+
     xlab("MISHTANE MIKRI")+ylab("density")+
     labs(caption = "for P(x) use cumulative=TRUE ")
+
   cat("Tohelet = ",toh,"\n")
   cat("Shonut =",sho,"\n")
   if(sort.prob){a=a[order(a[,2],decreasing = T), ,]}
   if(is.vector(x.range)) {cat("Probability for event between",
                               x.range[1],"and",x.range[2],"is",round(abs(pnorm(
                                 x.range[1],mean,sd)-pnorm(x.range[2],mean,sd)),round),"\n")}
-  print(g)
+  if(length(nudge==1)){nudge=c(nudge,nudge)}
+  ifelse(show.label,print(g+geom_label(aes(label=round(prob,round)),
+                                       alpha=0.8,nudge_x = nudge[1],nudge_y = nudge[2],color="white")),print(g))
   return(a)
 }
