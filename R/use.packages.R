cca3a07343d1419e4b52d8ffbf02110b7fff8937
bgupsych <95340431+bgupsych@users.noreply.github.com>
2022-02-12 17:01:06 +0200

    Add files via upload

---
+++ R/use.packages.R
@@ -0,0 +1,13 @@
+#' Install and/or call multiple packages
+#' @export
+#' @param packages a string of 1 or more packages names to be installed. Be aware of typos.
+#' @param library.only  A logical. When TRUE, function only calls required packages, otherwise, packages will be installed. Default is set to TRUE.
+#' @description Allows you to install and call from memory multiple packages at once.
+#' @note If you ask to re-install an existing package, a restart might be necessary. just press YES when asked by R. Otherwise abort and set \code{library.only} to TRUE.
+#' @examples use.package("tidyverse")
+#' @examples my_pack <- c("ggplot2","magrittr")
+#' use.package(my_pack,library.only=FALSE) # This will re-install these packages, and you will be asked to restart R.
+use.package <- function(packages,library.only=TRUE){
+  ifelse(library.only,"",lapply(packages,install.packages,character.only=T))
+  lapply(packages, require, character.only = T)
+}
