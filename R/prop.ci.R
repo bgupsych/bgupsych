e0cfaee9e72acb616aa56aee22d0559ff1fb2e2f
bgupsych <95340431+bgupsych@users.noreply.github.com>
2022-04-20 10:45:12 +0300

    Update prop.ci.R

--- R/prop.ci.R
+++ R/prop.ci.R
@@ -9,16 +9,21 @@
 #' @note  Calculates the binomal to normal assumption: \code{p&q * n > 5}.
 #' @returns \code{p} Is the succses rate, given \code{n} and \code{k}.
 #' @returns \code{q} Is the opposite probability of p (\code{1-p}).
+#' @return \code{SE} The Standard Error estimate based on empirical observations (\code{p and q}).
 #' @return \code{Lower} and \code{Upper} are the margins of the confidence interval.
 #' @return \code{conf} is the confidence level.
 #' @return \code{binom2norm} answers whether the assumption of normality is met.
 prop.ci <- function(n,k,conf=95){
   p <- k/n
-  q <- 1-p
-  Zscore <- qnorm((1-conf/100)/2,0,1)
-  prob <- sqrt((p*q)/n)
-  norma <- p*n & q*n >5
-  return(data.frame(p=round(p,2),q=round(q,2),Lower=round(p+Zscore*prob,3),
-                Upper=round(p-Zscore*prob,3), "conf"=round(conf),
-                "binom2norm"=ifelse(norma, "YES","NO")))
+  q <- 1 - p
+  Zscore <- qnorm((1 - conf/100)/2)
+  seest <- sqrt((p * q)/n)
+  norma <- p * n & q * n > 5
+
+  return(data.frame(p = round(p, 3),
+                    q = round(q, 3),SE = round(seest,3),
+                    Lower = round(p + Zscore * seest, 3),
+                    Upper = round(p - Zscore * seest, 3),
+                    conf = round(conf),
+                    binom2norm = ifelse(norma, "YES", "NO")))
 }
