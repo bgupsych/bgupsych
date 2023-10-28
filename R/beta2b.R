4d51316fcf7529bbaee273a36117520b9ed45bc1
Yann | BGUPsych <95340431+bgupsych@users.noreply.github.com>
2023-10-28 23:18:07 +0300

    Update beta2b.R
    adding the option to use sd instead of full vector

--- R/beta2b.R
+++ R/beta2b.R
@@ -2,8 +2,9 @@
 #' @export
 #' @description Converts the Beta value to a raw b, given the x and y
 #' @param beta The beta value you want to convert.
-#' @param x The x variable (Undependent).
+#' @param x The x variable (Independent).
 #' @param y The y variable (Dependent).
+#' @details You can either give as an input the full variable (a vector of length > 2) or the standard deviation.
 #' @returns raw Mekadem b
 #' @examples
 #' model = lm(Bsum~sat+age,data=depression)
@@ -11,7 +12,13 @@
 #' We want to convert them back to raw b:
 #' sat: beta2b(beta=0.5038211, x=depression$sat, y=depression$Bsum)
 #' ## 0.7899105
-#' age: beta2b(0.4673743,depression$age, y=depression$Bsum)
+#' age: beta2b(0.4673743,sd(depression$age), y=sd(depression$Bsum))
 #' ## 0.4072746
-beta2b <- function(beta,x,y)(beta*sd(y)/sd(x))
 
+beta2b <- function (beta, x, y){
+  res <- beta * sd(y)/sd(x)
+  if(length(x)==1){
+    res = beta * y/x
+  }
+  return(res)
+}
